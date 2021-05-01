6e406ab8102b059e8b56483e1af090ff6e261fa1
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-05-01 21:07:42 +0530

    Review 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, url_for, session, logging, request
 import pyrebase
 app = Flask(__name__)
 
@@ -15,6 +15,7 @@ firebaseConfig = {
     }
 firebase=pyrebase.initialize_app(firebaseConfig)
 auth=firebase.auth()
+db=firebase.database()
 
 @app.route('/')
 def index():
@@ -28,58 +29,168 @@ def login():
 def signup():
     return render_template("signup.html")
 
+@app.route('/patient')
+def patient():
+    return render_template('patient.html')
+
+@app.route('/doctor')
+def doctor():
+    return render_template('doctor.html')
+
+@app.route('/posebots')
+def posebots():
+    return render_template('posebots.html')
+
+@app.route('/posebot')
+def posebot():
+    return render_template('posebot.html')
+
+
+@app.route('/palming',methods=['POST','GET'])
+def palming():
+    url="https://teachablemachine.withgoogle.com/models/vKItIykyq/"
+    return render_template('posebot.html', url=url)
+
+@app.route('/trataka',methods=['POST','GET'])
+def trataka():
+    url="https://teachablemachine.withgoogle.com/models/YBbEKwztQ/"
+    return render_template('posebot.html', url=url)
+
+@app.route('/bhramamudra',methods=['POST','GET'])
+def bhramamudra():
+    url="https://teachablemachine.withgoogle.com/models/znvpEncRJ/"
+    return render_template('posebot.html', url=url)
+
+@app.route('/pranayama',methods=['POST','GET'])
+def pranayama():
+    url="https://teachablemachine.withgoogle.com/models/ZW47sepej/"
+    return render_template('posebot.html', url=url)
+
+@app.route('/parvatasana',methods=['POST','GET'])
+def parvatasana():
+    url="https://teachablemachine.withgoogle.com/models/JnfkxNVh3/"
+    return render_template('posebot.html', url=url)
+
 @app.route('/login/patient',methods=['POST','GET'])
 def login_patient():
+    flag=0
+    flag2=0
     int_features=[x for x in request.form.values()]
     print(int_features)
-    email=int_features[0]
-    password=int_features[1]
-    try:
-        auth.sign_in_with_email_and_password(email, password)
-        print("Patient Succesfully SignedIn")
-    except:
-        print("Invalid User Or Password")
+    name=int_features[0]
+    email=int_features[1]
+    password=int_features[2]
+    users = db.child("Patient").child(name).get()
+    if(users.each()!=None):
+        flag2=1
+        for user in users.each():
+            if(user.val()==email):
+                print('found')
+                flag=1
+    if(flag==1 and flag2==1):
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            print("Patient Succesfully SignedIn")
+            return render_template('patient.html', name=name) 
+        except:
+            print("Invalid User Or Password")
     return render_template('login.html')
 
 @app.route('/login/doctor',methods=['POST','GET'])
 def login_doctor():
+    flag=0
+    flag2=0
     int_features=[x for x in request.form.values()]
     print(int_features)
-    email=int_features[0]
-    password=int_features[1]
-    try:
-        auth.sign_in_with_email_and_password(email, password)
-        print("Doctor Succesfully SignedIn") 
-    except:
-        print("Invalid User Or Password")
+    name=int_features[0]
+    email=int_features[1]
+    password=int_features[2]
+    users = db.child("Doctors").child(name).get()
+    if(users.each()!=None):
+        flag2=1
+        for user in users.each():
+            if(user.val()==email):
+                print('found')
+                flag=1
+    if(flag==1 and flag2==1):
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            print("Doctor Succesfully SignedIn")
+            return render_template('doctor.html', name=name) 
+        except:
+            print("Invalid User Or Password")
     return render_template('login.html')
+
 
 @app.route('/signup/doctor',methods=['POST','GET'])
 def signup_doctor():
     int_features=[x for x in request.form.values()]
     print(int_features)
-    email=int_features[0]
-    password=int_features[1]
-    confirmpasswd=int_features[2]
-    try:
-        auth.create_user_with_email_and_password(email,password)
-        print("Doctor Created")
-    except:
-        print("Email already Exists")
+    name=int_features[0]
+    email=int_features[1]
+    password=int_features[2]
+    confirmpasswd=int_features[3]
+    designation=int_features[4]
+    hospital=int_features[5]
+    if(password==confirmpasswd):
+        try:
+            auth.create_user_with_email_and_password(email,password)
+            print("Doctor Created")
+            data={"name":name, "email": email, "designation":designation, "hospital":hospital }
+            print(email)
+            db.child("Doctors").child(name).set(data)
+            return render_template('doctor.html', name=name)
+        except:
+            print("Email already Exists")
     return render_template('signup.html')
 
 @app.route('/signup/patient',methods=['POST','GET'])
 def signup_patient():
     int_features=[x for x in request.form.values()]
     print(int_features)
-    email=int_features[0]
-    password=int_features[1]
-    try:
-        auth.create_user_with_email_and_password(email,password)
-        print("Patient Created")
-    except:
-        print("Email already Exists")
+    name=int_features[0]
+    email=int_features[1]
+    password=int_features[2]
+    confirmpassword=int_features[3]
+    height=int_features[4]
+    weight=int_features[5]
+    age=int_features[6]
+    if(password==confirmpassword):
+        try:
+            auth.create_user_with_email_and_password(email,password)
+            print("Patient Created")
+            data={"name":name, "email": email, "age":age, "height":height, "weight":weight}
+            print(email)
+            db.child("Patient").child(name).set(data)
+            return render_template('patient.html', name=name)
+        except:
+            print("Email already Exists")
     return render_template('signup.html')
+
+
+@app.route('/view_patients', methods=['POST','GET'])
+def view_patients():
+    patients=[]
+    users = db.child("Patient").get()
+    for user in users.each():
+        patients.append(user.val())
+    print(patients)
+    return render_template('patientlist.html', patients=patients)
+
+@app.route('/report', methods=['POST','GET'])
+def report():
+    patients=[]
+    users = db.child("Patient").get()
+    for user in users.each():
+        patients.append(user.val())
+    int_features=[x for x in request.form.values()]
+    print(int_features)
+    name=int_features[0]
+    report=int_features[1]
+    db.child("Patient").child(name).update({"report":report})
+    return redirect('/view_patients')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
